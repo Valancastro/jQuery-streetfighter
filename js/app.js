665d041b807bd727a2ddf0657f10906f99ab8944
unknown <castrovictor712@gmail.com>
2015-10-08 14:33:49 -0700

    jQuery modifications

--- js/app.js
+++ js/app.js
@@ -12,21 +12,32 @@ $(document).ready(function () {
          $('.ryu-ready').hide();
 
     }).mousedown(function() {
-        // play hadouken sound
+        playHadouken();
         $('.ryu-ready').hide();
         $('.ryu-throwing').show();
-        $('.hadouken').show(); 
-        // animate hadouken to the right of the screen
+        $(".hadouken").finish()
+        .show().animate(
+    	{'left':'1020px'}, 500,
+    	function() {
+    		$(this).hide();
+    		$(this).css('left', '559px');
+
+    	});
+
           
 
     }).mouseup(function(){
         $('.ryu-throwing').hide();
         $('.ryu-ready').show();
         $('.hadouken').hide();
-
         // ryu goes back to his ready position
     });
 
-
-
+    
 });
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
