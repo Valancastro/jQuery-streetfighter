ce3552634acbfe0f1e3d88e541dfbe93553b5e75
unknown <castrovictor712@gmail.com>
2015-10-08 13:21:14 -0700

    added jQuery finctions

--- js/app.js
+++ js/app.js
@@ -1,19 +1,32 @@
 $(document).ready(function () {
+
    $('.ryu').mouseenter(function() {
    	   
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
+        $('.ryu-still').hide();
+         $('.ryu-ready').show();
 
 
-   });
-   
-   $('.ryu').mouseleave(function() {
+    }).mouseleave(function() {
 
-   	$('.ryu-still').show();
-    $('.ryu-ready').hide();
+       	$('.ryu-still').show();
+         $('.ryu-ready').hide();
 
+    }).mousedown(function() {
+        // play hadouken sound
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').show(); 
+        // animate hadouken to the right of the screen
+          
 
-   });
+    }).mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+        $('.hadouken').hide();
+
+        // ryu goes back to his ready position
+    });
+
 
 
 });
